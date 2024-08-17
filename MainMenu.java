c16245babaf972a5551a1216bda517bad52fa971
Zenderland <jeffreydai666@gmail.com>
2024-08-16 23:59:48 -0400

    xd

--- MainMenu.java
+++ MainMenu.java
@@ -1,12 +1,17 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.lang.String;
-import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
-public class MainMenu {
+
+public class MainMenu extends JFrame{
+    private JFrame menu;
+    public MainMenu(){
+        menu = new JFrame();//initializes JFrame
+        menu.setBackground(Color.black);
+        menu.setTitle("Menu");
+        menu.setSize(580, 345);
+        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        menu.setResizable(false);
+        menu.setLayout(null);//guis go exactly where I put them
+        menu.setContentPane(new JLabel(new ImageIcon("C:\\Users\\jeffr\\OneDrive\\Documents\\GitHub\\ignitionhacks\\out\\worldmap.jpg")));
+        menu.setVisible(true);
+    }
 }
