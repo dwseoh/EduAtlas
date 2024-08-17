259a65d995475abd2a42ce139534c0c4542c568f
Zenderland <jeffreydai666@gmail.com>
2024-08-17 00:14:15 -0400

    s

--- MainMenu.java
+++ MainMenu.java
@@ -1,8 +1,17 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.String;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainMenu extends JFrame{
     private JFrame menu;
+    private JButton southSudan;
     public MainMenu(){
         menu = new JFrame();//initializes JFrame
         menu.setBackground(Color.black);
@@ -13,5 +22,17 @@ public class MainMenu extends JFrame{
         menu.setLayout(null);//guis go exactly where I put them
         menu.setContentPane(new JLabel(new ImageIcon("worldmap.jpg")));
         menu.setVisible(true);
+
+        southSudan = new JButton("South Sudan");
+        southSudan.setBounds(121,290,110,25);
+        southSudan.addActionListener(this);
+        southSudan.setFocusable(false);
+        southSudan.setPreferredSize(110,25);
+        southSudan.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
